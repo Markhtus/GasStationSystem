47714621cd7c3ba4a959693cdd9b0489b4422ede
Markhtus <markantoni13@outlook.com>
2025-06-03 16:20:23 -0300

    First commit

---
+++ AV1 Sistema de posto- Geovany, Pedro G., Marcus A. e Arthur S..cpp
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+#include <windows.h>
+
+int main() {
+    int formapagamento, parcelamento, abastecimento, litrosAbastecidos;
+    char respostapagamento[4], respostaabastecimento;
+    float valorAbastecimento;
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    setlocale(LC_ALL, "Portuguese");
+
+    char encerrar = ' ';
+    while (encerrar != 'S' && encerrar != 's') {
+        formapagamento = 0;
+        respostapagamento[0] = '\0';
+        int escolhaValida = 0;
+
+        while (!escolhaValida) {
+            printf("Olá! Seja bem-vindo ao posto Ipiranga\n\n");
+
+            printf("Indique a forma de pagamento, as opções são:\nDigite 1 caso seja Dinheiro. \nDigite 2 caso seja Pix. \nDigite 3 caso seja Cartão de crédito. \n");
+            scanf("%d", &formapagamento);
+
+            switch (formapagamento) {
+                case 1:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Você escolheu Dinheiro. A opção está correta? (sim / não): ");
+                    break;
+                case 2:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Você escolheu Pix. A opção está correta? (sim / não): ");
+                    break;
+                case 3:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Você escolheu Cartão de crédito. A opção está correta? (sim / não): ");
+                    break;
+                default:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Opção inválida. Por favor, escolha novamente.\n");
+                    while(getchar() != '\n');
+                    continue;
+            }
+
+            scanf("%s", respostapagamento);
+
+            if (respostapagamento[0] == 's' || respostapagamento[0] == 'S') {
+                printf("\nÓtimo! Você escolheu a opção %d.\n\n", formapagamento);
+                if (formapagamento == 3) {
+                    printf("Já que a opção selecionada foi Cartão de crédito, oferecemos o parcelamento em até três vezes. Em quantas vezes deseja parcelar? (1, 2 ou 3): ");
+                    scanf("%d", &parcelamento);
+                    printf("Ok, você escolheu parcelar em %d vezes\n", parcelamento);
+                    Sleep(1);
+                }
+                escolhaValida = 1;
+            } else {
+                printf("\nPor favor, escolha novamente a forma de pagamento.\n\n");
+            }
+        }
+
+        while (1) {
+            printf("Agora que já foi informada a forma de pagamento, você deseja abastecer por um valor específico ou por uma determinada quantidade de litros?\n Digite 1 para abastecer por um valor específico. \nDigite 2 para abastecer por uma determinada quantidade de litros\n");
+            scanf("%d", &abastecimento);
+
+            switch (abastecimento) {
+                case 1:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Você escolheu abastecer por um valor específico. A opção está correta? (sim/não): ");
+                    break;
+                case 2:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Você escolheu abastecer por uma determinada quantidade de litros. A opção está correta? (sim/não): ");
+                    break;
+                default:
+                    printf("\nProcessando...\n\n");
+                    Sleep(3);
+                    printf("Opção inválida. Por favor, escolha novamente.\n");
+                    continue;
+            }
+
+            scanf(" %c", &respostaabastecimento);
+
+            if (respostaabastecimento == 's' || respostaabastecimento == 'S') {
+                printf("\nPerfeito! Você escolheu abastecer da forma desejada.\n\n");
+
+                if (abastecimento == 1) {
+                    printf("Digite o valor em dinheiro que gostaria de abastecer: \n");
+                    scanf("%f", &valorAbastecimento);
+                    printf("Abastecimento de R$%.2f realizado com sucesso!\n", valorAbastecimento);
+                    litrosAbastecidos = 0;
+                } else {
+                    printf("Digite a quantidade de litros que gostaria de abastecer: ");
+                    scanf("%d", &litrosAbastecidos);
+                    printf("Abastecimento de %d litros realizado com sucesso!\n", litrosAbastecidos);
+                    valorAbastecimento = 0.0;
+                }
+
+                FILE *relatorio = fopen("relatório.txt", "a");
+
+                if (relatorio != NULL) {
+                    fprintf(relatorio, "Forma de pagamento: %d\n", formapagamento);
+                    if (abastecimento == 1) {
+                        fprintf(relatorio, "Abastecimento por valor específico: R$%.2f\n", valorAbastecimento);
+                    } else {
+                        fprintf(relatorio, "Abastecimento por quantidade de litros: %d litros\n", litrosAbastecidos);
+                    }
+                    fprintf(relatorio, "Data e hora do atendimento: %02d/%02d/%d -- %02d:%02d\n", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute);
+                    fprintf(relatorio, "------------------------------------\n");
+                    fclose(relatorio);
+                } else {
+                    printf("Erro ao abrir o arquivo de relatório.\n");
+                }
+
+                printf("Deseja encerrar o atendimento? (Digite 'S' para sair, qualquer outra tecla para continuar): ");
+                scanf(" %c", &encerrar);
+                if (encerrar == 'S' || encerrar == 's') {
+                    printf("Atendimento encerrado. Obrigado!\n");
+                    break;
+                } else {
+                    
+                    formapagamento = 0;
+                    respostapagamento[0] = '\0';
+                    escolhaValida = 0;
+                    break; 
+                }
+            } else {
+                printf("Por favor, escolha novamente a forma de abastecimento.\n");
+            }
+        }
+    }
+    return 0;
+}
+
